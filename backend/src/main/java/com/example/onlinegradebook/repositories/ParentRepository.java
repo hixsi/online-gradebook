0e8faa03d94d7e7523b9affa4da0d9a9709b2ba0
hixsi <77935852+hixsi@users.noreply.github.com>
2022-06-28 19:33:27 +0300

    Update ParentRepository.java

--- backend/src/main/java/com/example/onlinegradebook/repositories/ParentRepository.java
+++ backend/src/main/java/com/example/onlinegradebook/repositories/ParentRepository.java
@@ -4,4 +4,5 @@ import com.example.onlinegradebook.model.Parent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParentRepository extends JpaRepository<Parent, Integer> {
+   List<Parent> findAllByName(String name);
 }
