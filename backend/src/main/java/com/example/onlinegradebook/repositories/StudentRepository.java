54ac717bee884f74675e2980bfe31f53c68b25d3
hixsi <77935852+hixsi@users.noreply.github.com>
2022-06-28 19:34:57 +0300

    Update StudentRepository.java

--- backend/src/main/java/com/example/onlinegradebook/repositories/StudentRepository.java
+++ backend/src/main/java/com/example/onlinegradebook/repositories/StudentRepository.java
@@ -4,4 +4,5 @@ import com.example.onlinegradebook.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+  List<Student> findAllByName(String name);
 }
